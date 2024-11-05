ca6cf9796448865ee22bd8a1e11e556cf55be18e
hamm0208 <107385845+hamm0208@users.noreply.github.com>
2024-11-05 14:30:02 +0800

    Scene implementation

--- Scene.cpp
+++ Scene.cpp
@@ -5,3 +5,31 @@ Scene::Scene(): fName(""), fDescription(""), fPlayer(nullptr), fMonster(nullptr)
 
 Scene::Scene(string pName, string pDescription, Player* pPlayer, Monster* pMonster, int pMaxNumberOfEvent): fName(pName), fDescription(pDescription), fPlayer(pPlayer), fMonster(pMonster), fEvents(pMaxNumberOfEvent){
 }
+
+string Scene::getName() const{
+    return fName;
+};
+string Scene::getDescription() const{
+    return fDescription;
+};
+List<Decision> Scene::getDecisions() const{
+    return fDecisions;
+};
+Player* Scene::getPlayer(){
+    return fPlayer;
+};
+Monster* Scene::getMonster(){
+    return fMonster;
+};
+void Scene::AddDecisions(Decision& pDecision){
+    fDecisions.pushBack(pDecision)
+};
+void Scene::AddLoot(Item* pLoot){
+    fLoot.pushBack(pLoot);
+};
+void Scene::AttachScene(int pIndex, NTree<Scene, 3>* pSceneChild){
+    fSceneTree.attachNTree(pIndex, pSceneChild);
+};
+void Scene::DetachScene(int pIndex){
+    fSceneTree.detachNTree(pIndex);
+};
--- Scene.h
+++ Scene.h
@@ -15,19 +15,17 @@ class Scene
 		Stack<Event> fEvents;			//Events of the scene
 		Player* fPlayer;				//Player is in all the scene
 		Monster* fMonster;				//Monster is in all the scene
-		NTree<Scene*, 2> fSceneChilds;	//Childs of this scene class
+		NTree<Scene*, 2> fSceneTree;	//Childs of this scene class
 	public:
 		Scene();
 		Scene(string pName, string pDescription, Player* pPlayer, Monster* pMonster, int pMaxNumberOfEvent);
-		/*
 		string getName() const;
 		string getDescription() const;
 		List<Decision> getDecisions() const;
 		Player* getPlayer();
 		Monster* getMonster();
-		void AddDecisions();
-		void AddLoot();
+		void AddDecisions(Decision& pDecision);
+		void AddLoot(Item* pLoot);
 		void AttachScene(int pIndex, NTree<Scene, 3>* pSceneChild);
 		void DetachScene(int pIndex);
-		*/
 };
